5fb18d5fcf00a9f23fd580d3c0333668df280c79
nappdaddy2000 <mnapper3@ivytech.edu>
2017-02-12 04:54:10 +0000

    fix figaro names

--- config/environment.rb
+++ config/environment.rb
@@ -6,10 +6,10 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
-    :port => '2525',
+    :port => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => Figaro.env['SENDGRID_USERNAME'],
+    :password => Figaro.env['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
 }

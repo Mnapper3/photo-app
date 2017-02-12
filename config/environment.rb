bf3132e903b602a51356552372eec7ee81681c41
nappdaddy2000 <mnapper3@ivytech.edu>
2017-02-12 21:58:47 +0000

    changed evn

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,7 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
+  :domain => 'napper-photo-app.herokuapp.com',
   :enable_starttls_auto => true
 }
 
